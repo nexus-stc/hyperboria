e1783cf0832fcf4219f57523867a02af8ccfacce
the-superpirate <fist.of.the.first.pirates@gmail.com>
2021-01-07 15:09:52 +0300

    -  Open translations
    GitOrigin-RevId: bd0dedac08c982ccabacfb2e61f66aac35703acb

---
+++ nexus/translations/__init__.py
@@ -0,0 +1,19 @@
+from library.configurator import Configurator
+
+
+def get_translations():
+    return Configurator([
+        'nexus/translations/translations.yaml',
+    ])
+
+
+def t(label, language='en'):
+    if language in _translations and label in _translations[language]:
+        return _translations[language][label]
+    return _translations['en'][label]
+
+
+_translations = get_translations()
+
+
+__all__ = ['t']
